8d27b8c9f79c34a8be7b5b7be7358af2f60c12fa
orishaba-mercy <orishabamercybanga@gmail.com>
2023-05-06 13:01:19 +0300

    first

---
+++ pip.js
@@ -0,0 +1,15 @@
+// Given an array of strings, use a function to reverse all the elements in the 
+// string in ascending order and the specific elements in descending order.
+
+
+
+
+// Given an array of objects, each object representing a person with a name and age property, write a function that returns the sum of all people who are less than 18 years.
+// const people = [
+//   { name: 'Alice', age: 17 },
+//   { name: 'Eunice', age: 22 },
+//   { name: 'Charlie', age: 14 },
+//   { name: 'Max', age: 19 },
+// ];
+// Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+// Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
